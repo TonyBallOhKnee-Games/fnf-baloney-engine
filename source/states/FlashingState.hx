923ea35cb2cf76d6467f33425f1affa908adc69d
TonyBallOhKnee <128426022+TonyBallOhKnee-Games@users.noreply.github.com>
2024-07-05 18:59:33 -0600

    Update FlashingState.hx

--- source/states/FlashingState.hx
+++ source/states/FlashingState.hx
@@ -19,8 +19,8 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
+			"You know what's crazy?\n
+			This Modded Engine contains some flashing lights!\n
 			Press ENTER to disable them now or go to Options Menu.\n
 			Press ESCAPE to ignore this message.\n
 			You've been warned!",
